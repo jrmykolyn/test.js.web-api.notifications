efb5a8ea59d0e4234db70255b6f49dee79e0cbcb
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-01-24 00:42:16 -0500

    Added proof-of-concept function for initiating 'notification sequence'... probably shouldn't ever use it though.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,15 @@
                     )
                 break;
             case 'granted':
-                showNotification( 'Welcome back!' );
+                var msgs = [
+                    'This is the first message.',
+                    'This is the second message',
+                    'This is the third message',
+                    'This is the fourth message',
+                    'This is the fifth message'
+                ];
+
+                var sequenceIntervalId = initIntervalSequence( msgs, 1000 );
 
                 break;
             default:
@@ -36,5 +44,24 @@
         var n = new Notification( msg );
 
         setTimeout( function() { n.close(); }, 5000 );
+    }
+
+    function initIntervalSequence( msgs, frequency ) {
+        msgs = msgs || [];
+        frequency = frequency || 5000;
+
+        var counter = 0;
+        var interval = setInterval( function() {
+            if ( counter === msgs.length ) {
+                clearInterval( interval );
+                return;
+            }
+
+            showNotification( msgs[ counter ] );
+
+            counter++;
+        }, frequency );
+
+        return interval;
     }
 } )();
